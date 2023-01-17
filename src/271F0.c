9270e575a3b23561acdeedfde16f2407426a86eb
Mallos31 <Mallos42@gmail.com>
2023-01-17 13:38:01 -0500

    BGM code updates

--- src/271F0.c
+++ src/271F0.c
@@ -3,28 +3,57 @@
 extern u8 D_80053CAC;
 extern u8 D_8008FCC6;
 
+typedef struct {
+    s16 unk0;
+    s16 unk2;
+    s8 unk4;
+}unk53B00s;
+
+extern unk53B00s D_80053B00;
+
 #pragma GLOBAL_ASM("asm/nonmatchings/271F0/func_800265F0.s")
 
 #pragma GLOBAL_ASM("asm/nonmatchings/271F0/func_80026658.s")
 
 //#pragma GLOBAL_ASM("asm/nonmatchings/271F0/func_800267B8.s")
 void func_800267B8(s8 arg0) { //UpdateBGM
-    if (arg0 != D_8008FCC1) {
-        D_8008FCC1 = arg0;
-        D_8008FCC2 |= 1;
+    if (arg0 != gCurrentBGM) {
+        gCurrentBGM = arg0;
+        gPlayBGM |= 1;
     }
 }
 
 //#pragma GLOBAL_ASM("asm/nonmatchings/271F0/func_800267F8.s")
 void func_800267F8(s8 arg0, u16 arg1) {
-    if (arg0 != D_8008FCC1) {
-        D_8008FCC1 = arg0;
-        D_8008FCC2 |= 1;
-        D_8008FCC4 = arg1 & 0xFFFF;
+    if (arg0 != gCurrentBGM) {
+        gCurrentBGM = arg0;
+        gPlayBGM |= 1;
+        gBGMDelay = arg1 & 0xFFFF;
     }
 }
 
-#pragma GLOBAL_ASM("asm/nonmatchings/271F0/func_8002684C.s")
+//#pragma GLOBAL_ASM("asm/nonmatchings/271F0/func_8002684C.s")
+void func_8002684C(s32 arg0, s32 arg1, u16 arg2) {
+    s16 temp_a2;
+    s32 var_v1;
+    unk53B00s* var_v0;
+
+    var_v0 = &D_80053B00;
+
+    for (var_v1 = 0x46; var_v1 != 0; var_v1--) {
+        if ((arg0 == var_v0->unk0) && ((arg1 == var_v0->unk2) || (var_v0->unk2 == -1))) {
+            break;
+        }
+        var_v0++;
+    }
+    if (var_v1 != 0) {
+        if (gCurrentBGM != var_v0->unk4) {
+            gCurrentBGM = var_v0->unk4;
+            gPlayBGM |= 1;
+            gBGMDelay = arg2;
+        }
+    }
+}
 
 #pragma GLOBAL_ASM("asm/nonmatchings/271F0/func_800268D4.s")
 
