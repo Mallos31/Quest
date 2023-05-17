b1deab89040f0681dc7fff8179880f13f9f32e40
Mallos31 <Mallos42@gmail.com>
2023-05-17 17:44:48 -0400

    update config file for -g0

--- src/ovl_overlay1/DA68F0.c
+++ src/ovl_overlay1/DA68F0.c
@@ -1,6 +1,25 @@
 #include "common.h"
 
-#pragma GLOBAL_ASM("asm/nonmatchings/ovl_overlay1/DA68F0/func_80100000_DA68F0.s")
+extern s32 D_80103420_DA9D10;
+extern s32 D_80103424_DA9D14;
+extern s32 D_8011AD30;
+extern s32 D_8011AD34;
+extern s32 D_8011AD38;
+extern s32 D_8011AD3C;
+extern s32 D_8011AD40;
+extern s32 D_8011AD44;
+
+//#pragma GLOBAL_ASM("asm/nonmatchings/ovl_overlay1/DA68F0/func_80100000_DA68F0.s")
+void func_80100000_DA68F0(void) {
+    D_8011AD30 = 0;
+    D_8011AD44 = 0;
+    D_80103424_DA9D14 = 1;
+    D_80103420_DA9D10 = 0;
+    D_8011AD34 = 1;
+    D_8011AD3C = 0;
+    D_8011AD40 = 8;
+    D_8011AD38 = 0;
+}
 
 #pragma GLOBAL_ASM("asm/nonmatchings/ovl_overlay1/DA68F0/func_8010004C_DA693C.s")
 
